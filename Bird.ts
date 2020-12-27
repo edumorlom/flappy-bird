083ad57b2dc5450b0843f32ddda3e7e6b648eb73
Eduardo Morales <edumorales@google.com>
2020-12-27 21:19:32 +0100

    updates

--- Bird.ts
+++ Bird.ts
@@ -1,4 +1,5 @@
-const BIRDIMAGE = "./resources/bird.png"
+/* eslint-disable require-jsdoc */
+const BIRDIMAGE = './resources/bird.png';
 
 export default class Bird {
     altitude = 45
@@ -7,44 +8,44 @@ export default class Bird {
     rotation = 1
 
     constructor(xAxisInPct?: number) {
-        // Given a percentage, where 0 is the most left and 100
-        // is the most right of a screen, place the bird in the xAxis.
-        if (xAxisInPct) {
-            this.xAxisInPct = xAxisInPct
+      // Given a percentage, where 0 is the most left and 100
+      // is the most right of a screen, place the bird in the xAxis.
+      if (xAxisInPct) {
+        this.xAxisInPct = xAxisInPct;
+      }
+
+      // Set the attributes of the bird.
+      this.HTMLElement.id = 'bird';
+      this.HTMLElement.style.left = this.xAxisInPct + '%';
+      this.HTMLElement.setAttribute('src', BIRDIMAGE);
+
+      // Every 1ms, move down 0.1%.
+      setInterval(() => {
+        this.setAltitude(this.altitude + 0.3);
+        if (this.rotation < 90) {
+          this.rotation += 0.5;
         }
-
-        // Set the attributes of the bird.
-        this.HTMLElement.id = "bird"
-        this.HTMLElement.style.left = this.xAxisInPct + "%"
-        this.HTMLElement.setAttribute("src", BIRDIMAGE)
-
-        // Every 1ms, move down 0.1%.
-        setInterval(() => {
-            this.setAltitude(this.altitude + 0.3)
-            if (this.rotation < 90) {
-                this.rotation += 0.5
-            }
-            this.HTMLElement.style.transform = `rotate(${this.rotation}deg)`
-        }, 1)
+        this.HTMLElement.style.transform = `rotate(${this.rotation}deg)`;
+      }, 1);
     }
 
-    flyUp = (): void => {
-        if (this.rotation <= 90) {
-            this.rotation = 0
-        }
-        this.HTMLElement.style.transform = `rotate(${this.rotation}deg)`
+    flyUp() {
+      if (this.rotation <= 90) {
+        this.rotation = 0;
+      }
+      this.HTMLElement.style.transform = `rotate(${this.rotation}deg)`;
 
-        const a = setInterval(() => {
-            this.setAltitude(this.altitude - 0.5)
-        }, 1)
+      const a = setInterval(() => {
+        this.setAltitude(this.altitude - 0.5);
+      }, 1);
 
-        setTimeout(() => {
-            clearTimeout(a)
-        }, 150)
+      setTimeout(() => {
+        clearTimeout(a);
+      }, 150);
     }
 
-    setAltitude = (altitude: number) => {
-        this.altitude = altitude
-        this.HTMLElement.style.top = altitude + "%"
+    setAltitude(altitude: number) {
+      this.altitude = altitude;
+      this.HTMLElement.style.top = altitude + '%';
     }
 }
--- index.ts
+++ index.ts
@@ -1,99 +1,102 @@
 // USE `tsc --module esnext  --watch *.ts`
 
-import Bird from './Bird.js'
-import Pipe from './Pipe.js'
-import Config from './Config.js'
+import Bird from './Bird.js';
+import Pipe from './Pipe.js';
+import Config from './Config.js';
 
 type positionType = Pipe | null;
 
 let score = 0;
-const scoreHTMLElement = document.getElementById("score")
+const scoreHTMLElement = document.getElementById('score');
 
-const birdXAxisInPct = (Config.birdPositionOnGrid / Config.totalPlaces) * 100
-const bird = new Bird(birdXAxisInPct)
+const birdXAxisInPct = (Config.birdPositionOnGrid / Config.totalPlaces) * 100;
+const bird = new Bird(birdXAxisInPct);
 
 // The map of the game contains n places.
 // Each place starts off empty spaces, but is then filled as the game goes on.
-const map: positionType[] = new Array(Config.totalPlaces)
+const map: positionType[] = new Array(Config.totalPlaces);
 
 // The sky element in the dom.
-const skyHTMLElement = document.getElementsByClassName("sky")[0]
+const skyHTMLElement = document.getElementsByClassName('sky')[0];
 
 // The width of the pipe in percent. 100% is the entire screen.
-const pipeWidthInPct = (1 / Config.totalPlaces) * 100
+const pipeWidthInPct = (1 / Config.totalPlaces) * 100;
 
 const isGameOn = (): boolean => {
+  // Is the bird out of bounds? Bounds are 0% to 100% in height.
+  const birdOutOfBounds = bird.altitude <= 0 || bird.altitude >= 100;
 
-    // Is the bird out of bounds? Bounds are 0% to 100% in height.
-    const birdOutOfBounds = bird.altitude <= 0 || bird.altitude >= 100
+  if (birdOutOfBounds) {
+    return false;
+  }
 
-    if (birdOutOfBounds) {
-        return false
-    }
+  // Grab the bird's position in the map, is there a pipe?
+  const position: positionType = map[Config.birdPositionOnGrid];
 
-    // Grab the bird's position in the map, is there a pipe?
-    const position: positionType = map[Config.birdPositionOnGrid]
+  // If the position the bird on doesn't contain a pipe, game hasn't ended.
+  if (position == null) {
+    return true;
+  }
 
-    // If the position the bird on doesn't contain a pipe, game hasn't ended.
-    if (position == null) {
-        return true
-    }
+  // Has the bird collided? Is the pipe on top or bottom of screen?
+  let birdCollision = false;
 
-    // Has the bird collided? Is the pipe on top or bottom of screen?
-    let birdCollision = false;
+  if (position.topOrBottom === 'top') {
+    birdCollision = bird.altitude < position.height;
+  } else {
+    birdCollision = bird.altitude > (100 - pipeWidthInPct) - position.height;
+  }
 
-    if (position.topOrBottom === "top") {
-        birdCollision = bird.altitude < position.height
-    } else {
-        birdCollision = bird.altitude > (100 - pipeWidthInPct) - position.height
-    }
-
-    return !birdCollision
-}
-
-setInterval(() => {
-    skyHTMLElement.innerHTML = ""
-    map.forEach((position, index) => {
-        if (position !== null) {
-            const pipeHTMLElement = position.HTMLElement
-            const xAxisInPct = (index / Config.totalPlaces) * 100
-            pipeHTMLElement.style.left = xAxisInPct + "%"
-            pipeHTMLElement.setAttribute("key", String(index))
-            skyHTMLElement.appendChild(pipeHTMLElement)
-        }
-    })
-    skyHTMLElement.appendChild(bird.HTMLElement)
-}, 1)
+  return !birdCollision;
+};
 
 setInterval(() => {
-    // Decides whether to insert a pipe or an empty space.
-    const emptyOrPipe = Math.random() >= (Config.pipeProbabilityInPct / 100)
-
-    // If emptyOrPipe, create a new Pipe, otherwise it's an empty space.
-    const space = emptyOrPipe ? new Pipe(pipeWidthInPct, Config.maxPipeHeightInPct) : null
-
-    // Place the new space to the map and get rid of index 0.
-    // This will create an effect of moving forward.
-    map.push(space)
-    map.shift()
-
-    // Every iteration, add 1 to the score.
-    score += 1
-
-    // Update the score on the DOM.
-    if (scoreHTMLElement) {
-        scoreHTMLElement.innerHTML = String(score)
+  skyHTMLElement.innerHTML = '';
+  map.forEach((position, index) => {
+    if (position !== null) {
+      const pipeHTMLElement = position.HTMLElement;
+      const xAxisInPct = (index / Config.totalPlaces) * 100;
+      pipeHTMLElement.style.left = xAxisInPct + '%';
+      pipeHTMLElement.setAttribute('key', String(index));
+      skyHTMLElement.appendChild(pipeHTMLElement);
     }
+  });
+  skyHTMLElement.appendChild(bird.HTMLElement);
+}, 1);
 
-    // If bird has died, display message.
-    const gameOver = !isGameOn()
-    if (gameOver) {
-        alert(`You have died! Your score is ${score}`)
-        location.reload()
-    }
-}, Config.startSpeed)
+setInterval(() => {
+  // Decides whether to insert a pipe or an empty space.
+  const emptyOrPipe = Math.random() >= (Config.pipeProbabilityInPct / 100);
+
+  let space = null;
+
+  // If emptyOrPipe, create a new Pipe, otherwise it's an empty space.
+  if (emptyOrPipe) {
+    space = new Pipe(pipeWidthInPct, Config.maxPipeHeightInPct)
+  }
+
+  // Place the new space to the map and get rid of index 0.
+  // This will create an effect of moving forward.
+  map.push(space);
+  map.shift();
+
+  // Every iteration, add 1 to the score.
+  score += 1;
+
+  // Update the score on the DOM.
+  if (scoreHTMLElement) {
+    scoreHTMLElement.innerHTML = String(score);
+  }
+
+  // If bird has died, display message.
+  const gameOver = !isGameOn();
+  if (gameOver) {
+    alert(`You have died! Your score is ${score}`);
+    location.reload();
+  }
+}, Config.startSpeed);
 
 // Every time the user presses a key, the bird flies up.
-document.addEventListener("keypress", () => {
-    bird.flyUp()
-})
+document.addEventListener('keypress', () => {
+  bird.flyUp();
+});
